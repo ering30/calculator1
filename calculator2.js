50151c524639c747d0e9cbbc80b900737e930292
ering30 <115221464+ering30@users.noreply.github.com>
2022-10-12 17:15:15 +1100

    update1

--- calculator2.js
+++ calculator2.js
@@ -18,18 +18,18 @@ function addButton(){
 function subtractButton(){
     var num1 = getInputValue1()
     var num2 = getInputValue2()
-    x = Number(firstNumber) - Number(secondNumber);
+    x = Number(num1) - Number(num2);
     document.getElementById("result").innerHTML= x;
 }
 function multiplyButton(){
     var num1 = getInputValue1()
     var num2 = getInputValue2()
-    x = Number(firstNumber) * Number(secondNumber);
+    x = Number(num1) * Number(num2);
     document.getElementById("result").innerHTML= x;
 }
 function divideButton(){
     var num1 = getInputValue1()
     var num2 = getInputValue2()
-    x = Number(firstNumber) / Number(secondNumber);
+    x = Number(num1) / Number(num2);
     document.getElementById("result").innerHTML= x;
 }
